9a3ba86688db0b1bd2aa59874e992377587bb5a8
Chris Bradley <chris.bradley@cy.id.au>
2025-06-23 22:42:50 +1000

    Add defrosting alert

--- custom_components/actronair_nimbus/binary_sensor.py
+++ custom_components/actronair_nimbus/binary_sensor.py
@@ -29,6 +29,9 @@ async def async_setup_entry(
                 ActronAirNimbusCleanFilterAlertBinarySensor(
                     coordinator, state, unique_id
                 ),
+                ActronAirNimbusDefrostingAlertBinarySensor(
+                    coordinator, state, unique_id
+                ),
             ]
         )
 
@@ -117,6 +120,44 @@ class ActronAirNimbusCleanFilterAlertBinarySensor(
     def _update_internal_state(self, state):
         """Update the internal state from the coordinator data."""
         self._attr_is_on = state._state["Alerts"]["CleanFilter"]
+
+
+class ActronAirNimbusDefrostingAlertBinarySensor(
+    ActronAirNimbusEntity, BinarySensorEntity
+):
+    """Representation of an Actron Air Nimbus binary sensor."""
+
+    _attr_translation_key = "defrosting_alert"
+    _attr_device_class = BinarySensorDeviceClass.PROBLEM
+
+    def __init__(
+        self,
+        coordinator,
+        initial_state,
+        ac_serial: str,
+    ) -> None:
+        super().__init__(coordinator, ac_serial)
+
+        self.ac_serial = ac_serial
+
+        self._attr_unique_id = f"{ac_serial}_{self._attr_translation_key}"
+        self._attr_device_info = DeviceInfo(
+            identifiers={(DOMAIN, ac_serial)},
+        )
+
+        self._update_internal_state(initial_state)
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        state = self.coordinator.data[self.ac_serial]
+
+        self._update_internal_state(state)
+
+        return super()._handle_coordinator_update()
+
+    def _update_internal_state(self, state):
+        """Update the internal state from the coordinator data."""
+        self._attr_is_on = state._state["Alerts"]["Defrosting"]
 
 
 class ActronAirNimbusZoneSensorConnectedBinarySensor(
